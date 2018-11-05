90eac0f60e2b85391aee41da117b826fd6ab5aa4
fausto <faustomontiel98@gmail.com>
2018-11-05 04:03:55 -0300

    tp

---
+++ Montiel.Fausto.2D.TP3/Archivos/Texto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto :IArchivo<string>
+    {
+        public bool Guardar(string archivo,string datos)
+        {
+            StreamWriter file = null;
+            bool salida = true;
+
+            try
+            {
+                file = new StreamWriter(archivo,false);
+                file.Write(datos);
+            }
+            catch(Exception)
+            {
+                salida = false;
+            }
+            finally
+            {
+                file.Close();
+
+            }
+            return salida;
+        }
+
+        public bool Leer(string archivo,out string datos)
+        {
+            StreamReader file = null;
+            bool salida = true;
+
+            try
+            {
+                file = new StreamReader(archivo);
+                datos = file.ReadToEnd();
+            }
+            catch(Exception)
+            {
+                datos = null;
+                salida = false;
+            }
+            finally
+            {
+                file.Close();
+            }
+            return salida;
+        }
+    }
+}
